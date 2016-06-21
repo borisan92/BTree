6e63f5360621fd39e10509851d8d9f185b4f88d1
borisan92 <borischap@gmail.com>
2016-06-21 12:55:30 +0300

    создание алгоритма сортировки двоичным деревом

--- js/tree.js
+++ js/tree.js
@@ -1,5 +1,51 @@
 /**
 * @author bchaplygin
-* @since 20.06.2016.
+* @date 20.06.2016.
 */
 
+$(function() {
+    var Tree = function (value, color) {
+        this.rightNode = null;
+        this.leftNode = null;
+        this.value = value;
+        this.color = color;
+    };
+
+    Tree.prototype.insert = function (value) {
+        var node = '';
+        var isRightNode = false;
+        if (this.value < value) {
+            node = 'rightNode';
+            isRightNode = true;
+        } else {
+            node = 'leftNode'
+        }
+
+        if (this[node] != null) {
+            this[node].insert(value)
+        } else {
+            this[node] = new Tree(value, isRightNode ? 'green' : 'orange');
+
+
+            if (this.children === undefined) {
+                this.children = [];
+            }
+            isRightNode ? this.children.push(this[node]) : this.children.unshift(this[node]);
+        }
+    };
+
+    Tree.prototype.traverse = function (func) {
+        if (this.leftNode != null) {
+            this.leftNode.traverse(func)
+        }
+
+        if(typeof func === 'function') {
+            func(this)
+        }
+
+        if (this.rightNode != null) {
+            this.rightNode.traverse(func);
+        }
+    };
+
+});
